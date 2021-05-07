71eb925837ce652b3ed245b9daa1aff3fd9c4ff4
yashmalviya17 <yashmalviya17@gmail.com>
2021-05-07 17:52:47 +0530

    component added with few changes

---
+++ src/components/global-components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from '../../assets/logo.png'
+const Navbar = () => {
+    return (
+        <div className='navbar' >
+           <span>
+             <img  src={logo} alt="p-logo" />
+           </span>
+           <span>
+                
+           </span>
+        </div>
+    )
+}
+
+export default Navbar
--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import { DataContextApi } from "../global-components/ContextApi";
+import { Col, Row } from "antd";
+import Container from "../global-components/Container";
+import "./homepage-style.css";
+import WaveSvg from "../global-components/WaveSvg";
 const HomePage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const { loading, setLoading, profileData, setProfileData } = useContext(
+    DataContextApi
+  );
 
-export default HomePage
+  return (<section className='main-section-hero'>
+      <div className='main-div-hero' >
+          <h1>
+              Professional Profile
+          </h1> 
+          <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam nihil consequatur tempore voluptates esse perspiciatis eligendi soluta aperiam voluptas, rem natus aut? Minima nesciunt temporibus obcaecati illum, officia porro.
+          </p>
+      </div>
+  </section>
+   
+  );
+};
+
+export default HomePage;
