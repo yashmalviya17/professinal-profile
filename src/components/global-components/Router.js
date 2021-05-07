c12d4c69b4fa1e74b7a3b4b61246fb29cd7c4a31
yashmalviya17 <yashmalviya17@gmail.com>
2021-05-07 17:52:17 +0530

    navbar add to route page so that every route can access it

--- src/components/global-components/Router.js
+++ src/components/global-components/Router.js
@@ -3,9 +3,11 @@ import { Switch, Route } from "react-router-dom";
 import ErrorPage from "./ErrorPage";
 import HomePage from "../home-page/HomePage";
 import SingleProfile from "../single-profile/SingleProfile";
+import Navbar from "./Navbar";
 const Router = () => {
   return (
     <div>
+      <Navbar/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/profile/:name/q?=:slug" component={SingleProfile} />
