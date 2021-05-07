33bec84a9db8f61c3126f4d6989977381d505d21
yashmalviya17 <yashmalviya17@gmail.com>
2021-05-07 10:10:36 +0530

    removed useless code

---
+++ src/contextapi/ContextApi.js
@@ -0,0 +1,17 @@
+import React,{useEffect} from 'react'
+import axios from 'axios'
+
+const ContextApi = (props) => {
+
+    useEffect(()=>{
+     axios.
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ContextApi
